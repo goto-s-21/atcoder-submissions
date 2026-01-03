76b2e866280351abeac2f454007582a6ac8601f4
goto-s-21 <mone08sh21@gmail.com>
2026-01-03 00:48:32 +0000

    Update AtCoder submissions

---
+++ abc/abc435/abc435_a/71769584.py
@@ -0,0 +1,7 @@
+N = int(input())
+
+line = 0
+for i in range(N + 1):
+  line += i
+    
+print(line)
---
+++ abc/abc436/abc436_a/71755733.py
@@ -0,0 +1,8 @@
+N = int(input())
+S = input()
+
+S_len = len(S)
+
+d = N - S_len
+
+print(d * "o" + S)
